fccd9be30878edf8fbc5ec9822ee705af81358e7
sirmantris <sandro.calzada@gmail.com>
2017-06-26 20:24:14 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,4 +1,7 @@
   server = function(input, output) {
-    source("C:/Users/SANDRO/Documents/shiny_blutaca/fserver.R"), #cambiar ruta
-    output$Recomendacion <- renderTable({pela[[2]]}, rownames = FALSE, colnames = FALSE)
+    source("C:/Users/SANDRO/Documents/shiny_blutaca/fserver.R")
+    #output$Recomendacion <- renderTable({input$RecomPel}, rownames = FALSE, colnames = FALSE)
+    output$Recomendacion <- renderTable({x[x[,2] %in% input$RecomPel,] }, rownames = FALSE, colnames = FALSE)
   }
+  
+
